a53170214a9af593dce0cd638ed0698d44775274
l1f16bscs0324 <rashidmehmood@ucp.edu.pk>
2019-04-30 10:58:22 +0500

    lec 15

--- lec 15/script.js
+++ lec 15/script.js
@@ -9,3 +9,51 @@ b1.addEventListener("mouseenter",function () {
     console.log("mouse entered !")
 })*/
 
+var button = document.getElementById("enter");
+var input = document.getElementById("user-input");
+var ul = document.getElementsByTagName("ul")[0];
+
+function inputlength(){
+    if(input.value.length > 0) {
+
+        return true;
+        return false;
+    }
+
+
+}
+button.addEventListener("click",function () {
+
+});
+
+
+function additemafterclick(){
+    if(inputlength()){
+        createlistitem();
+    }
+}
+function additemafterkeypress(e){
+
+}
+button.addEventListener("click",function () {
+    if(input.value.length > 0){
+        var li = document.createElement("li");
+        li.append(document.createTextNode(input.value));
+        ul.append(li);
+        input.value = '';
+    }
+});
+
+
+
+input.addEventListener("keypress",function (e) {
+    console.log(e);
+    if(input.value.length > 0 && e.which===13){
+        var li = document.createElement("li");
+        li.append(document.createTextNode(input.value));
+        ul.append(li);
+        input.value = '';
+    }
+});
+
+
